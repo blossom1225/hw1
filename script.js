cfb4bc4c3e1c0efcb0d9941545c923799f987b5f
blossom1225 <96513942+blossom1225@users.noreply.github.com>
2023-03-06 22:12:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let count = 10;
-const  countdown = document.getElementById("countdown");
+const  countdown = document.getElementById("count");
 const  countdownInterval = setInterval(function() {
     if (count > 0) {
         countdown.innerHTML =  + count + " seconds" + " reload";
         count--;
     } else {
         clearInterval(countdownInterval);
-        countdown.innerHTML = "reloading";
+        countdown.innerHTML = "reloading...";
     }
 }, 1000);

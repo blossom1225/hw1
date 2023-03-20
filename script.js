c8a1f28870b16c3fcd62460f75495984a1b2bbb2
blossom1225 <96513942+blossom1225@users.noreply.github.com>
2023-03-20 14:29:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let count = 10;
 const  countdown = document.getElementById("count");
 const  countdownInterval = setInterval(function() {
-    if (count > 0) {
+    if (count >= 0) {
         countdown.innerHTML =  + count + " seconds" + " reload";
         count--;
     }
